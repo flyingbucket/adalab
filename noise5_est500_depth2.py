16c34da3e4925fd1fb16d631b681aa55c0883942
flyingbucket <menglinli598@gmail.com>
2025-11-25 22:53:10 +0800

    exp:first pair of baseline and noise,both on MNIST

---
+++ noise5_est500_depth2.py
@@ -0,0 +1,6 @@
+from src.utils import train_and_save, load_compressed
+
+if __name__ == "__main__":
+    clf, monitor, (X_train, X_test, y_train, y_test, noise_idx, clean_idx), paths = (
+        train_and_save("./configs/noise5_est500_depth2_v1.json")
+    )
