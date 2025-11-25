89cf12523db9333dd4f1b945a3b8a1c29fdb709c
Miller <2311067@mail.nankai.edu.cn>
2025-11-25 17:33:46 +0800

    demo of training wrapper

---
+++ demo_wrapper.py
@@ -0,0 +1,13 @@
+from src.utils import train_and_save, load_compressed
+
+if __name__ == "__main__":
+    # === 用包装函数训练并保存 ===
+    clf, monitor, (X_train, X_test, y_train, y_test, noise_idx, clean_idx), paths = (
+        train_and_save("./configs/test_experiment_wrapper.json")
+    )
+
+    # === 加载压缩版模型和 monitor ===
+    clf_loaded = load_compressed(paths["compressed_clf"])
+    monitor_loaded = load_compressed(paths["compressed_monitor"])
+
+    print("模型加载成功")
